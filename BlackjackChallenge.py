a2cf894b4ecf35a5b969b84fdc08376749cbd6a1
Nathan Jorgensen <nathan.jorgensen@justice.qld.gov.au>
2024-08-28 15:36:13 +1000

    BJ challenge method complete - needs debugging

--- BlackjackChallenge.py
+++ BlackjackChallenge.py
@@ -3,19 +3,17 @@ from BlackjackHand import BlackjackHand
 
 class BlackjackChallenge():
 
-    def __init__(self, num_decks=6, bet=1.0, max_hands=3):
+    def __init__(self, num_decks=6, max_hands=3):
         self.deck = Deck(num_decks)
         self.deck.shuffle()
         
         # game attributes
-        self.bet = bet
         self.max_hands = max_hands
 
         #game initialisation
-        self.player_bet = []
-        self.amount_won = 0.0
+        self.game_win = 0.0
 
-        self.hands = [BlackjackHand(self.draw_card(number_of_cards=2))]
+        self.player_hands = [BlackjackHand(self.draw_card(number_of_cards=2))]
         self.dealer_hand = BlackjackHand(self.draw_card(number_of_cards=2))
 
     def print_hand(self, hand):
@@ -24,20 +22,48 @@ class BlackjackChallenge():
     
     def draw_card(self, number_of_cards=1):
         return [self.deck.draw_card() for _ in range(number_of_cards)]
-    
+      
     @property
     def dealer_first_card_rank(self):
         return self.dealer_hand.card_ranks[0]
     
-    def five_card_charlie(self, hand: BlackjackHand):
-        return len(hand.hand) == 5 and hand.hand_value <= 21
+    @property
+    def money_on_table(self):
+        return sum(hand.wagered_amount for hand in self.player_hands)
+    
+    def blackjack_pays(self, hand: BlackjackHand):
+        if not self.dealer_hand.is_hand_blackjack:
+            return 2.0
+        if hand.blackjack_rank < self.dealer_hand.blackjack_rank:
+            return 3.0
+        if hand.blackjack_rank == self.dealer_hand.blackjack_rank:
+            return 4.0
+        if hand.blackjack_rank > self.dealer_hand.blackjack_rank:
+            return 5.0 
+ 
+    def calculate_payout(self, hand: BlackjackHand):
+        if hand.is_hand_busted:
+            return -1.0
+        elif hand.is_hand_blackjack:
+            return self.blackjack_pays(hand)
+        elif hand.is_five_card_charlie:
+            return 1.0
+        elif hand.hand_value == 21:
+            return 1.0
+        elif self.dealer_hand.is_hand_busted:
+            return 1.0
+        elif hand.hand_value > self.dealer_hand.hand_value:
+            return 1.0
+        
+        # all other hands lose
+        return -1.0
 
     def dealer_play(self) -> None:
         while self.dealer_hand.hand_value < 17:
             self.dealer_hand.add_card(self.draw_card())
 
     def can_split(self, hand: BlackjackHand):        
-        return hand.can_split()
+        return hand.can_split
     
     def does_player_split(self, hand: BlackjackHand):
         if not self.can_split(hand):
@@ -135,50 +161,97 @@ class BlackjackChallenge():
             if hand.hand_value >= 18:
                 return True
         
+        if len(hand.hand) == 5:
+            return True
+        
         return False
             
-
-
     def split_player_hand(self, hand: BlackjackHand):
         new_hand = BlackjackHand([hand.hand.pop()])
         hand.add_card(self.draw_card())
         new_hand.add_card(self.draw_card())
-        self.hands.append(new_hand)
+        self.player_hands.append(new_hand)
     
     def double_down(self, hand: BlackjackHand):
+        hand.wagered_amount *= 2
         hand.add_card(self.draw_card())
-
     
     def optimal_strategy(self, hand: BlackjackHand):
         while True:
-            if self.does_player_split(hand) and len(self.hands) < self.max_hands:
+            if self.does_player_split(hand) and len(self.player_hands) < self.max_hands:
                 self.split_player_hand(hand)
-                print('split')
             elif self.does_player_double(hand):
                 self.double_down(hand)
-                print('double')
                 break
             elif self.does_player_stand(hand):
-                print('stand')
                 break
             else:
-                print('hit')
                 hand.add_card(self.draw_card())  
 
     def play(self):
         hand_index = 0
-        while hand_index < len(self.hands):
-            self.optimal_strategy(self.hands[hand_index])
+        while hand_index < len(self.player_hands):
+            self.optimal_strategy(self.player_hands[hand_index])
             hand_index += 1
+        
+    def is_hand_auto_resolved(self, hand: BlackjackHand):
+        if hand.is_hand_blackjack:
+            return True
+        if hand.is_five_card_charlie:
+            return True
+        if hand.hand_value == 21:
+            return True
+        if hand.is_hand_busted:
+            return True
+        return False
+    
+    def amount_not_auto_resolved(self):       
+        return sum(hand.wagered_amount for hand in self.player_hands if not self.is_hand_auto_resolved(hand))
 
 
+    def win(self):
+        
+        unresolved_amount = self.amount_not_auto_resolved()
 
+        for hand in self.player_hands:
+            if self.is_hand_auto_resolved(hand):
+                self.set_hand_winnings(hand)
+            elif self.dealer_hand.is_hand_blackjack:  
+                unresolved_amount = self.handle_blackjack_unresolved(hand, unresolved_amount)
+            else:
+                self.set_hand_winnings(hand)
 
+    def set_hand_winnings(self, hand: BlackjackHand):
+        hand.amount_won = hand.wagered_amount * self.calculate_payout(hand)
 
-game = BlackjackChallenge()
+    def handle_blackjack_unresolved(self, hand: BlackjackHand, unresolved_amount):
 
-game.play()
-game.dealer_play()
+        if unresolved_amount > hand.wagered_amount:
+            unresolved_amount -= hand.wagered_amount
+        else:
+            if hand.has_doubled:
+                hand.wagered_amount = 1.0
+            self.set_hand_winnings(hand)
 
-print(game.hands[0])
-print(game.dealer_hand)
+        return unresolved_amount
+
+if __name__ == "__main__":
+
+    num_hands = 1000000
+
+    amount_won = 0
+    amount_bet = 0
+
+    for iter in range(1, num_hands+1):
+        game = BlackjackChallenge()
+        game.play()
+        game.dealer_play()
+        game.win()
+
+        amount_won_hand = sum(hand.amount_won for hand in game.player_hands)
+        amount_bet_hand = sum(hand.wagered_amount for hand in game.player_hands)
+
+        amount_won += amount_won_hand
+        amount_bet += amount_bet_hand
+
+    print(num_hands, ' ', amount_bet, ' ', amount_won, ' ', amount_won/num_hands)
--- BlackjackHand.py
+++ BlackjackHand.py
@@ -1,9 +1,11 @@
 from Cards import Card
 
-
 class BlackjackHand:
+
     def __init__(self, hand: list[Card]=None) -> None: 
         self.hand = hand if hand else []
+        self.wagered_amount = 1.0
+        self.amount_won = 0.0
         self.update_hand_value()
 
     def __str__(self):
@@ -42,8 +44,38 @@ class BlackjackHand:
         return self._hand_value == 21 and len(self.hand) == 2
     
     @property
+    def is_hand_busted(self):
+        return self._hand_value > 21
+    
+    @property
+    def is_five_card_charlie(self):
+        return len(self.hand) == 5 and not self.is_hand_busted
+
+    @property
+    def blackjack_rank(self):
+        if not self.is_hand_blackjack:
+            return None
+        
+        # Assign rank based on the 10-value card
+        rank_values = {
+            '10': 1, 'J': 2, 'Q': 3, 'K': 4
+        }
+
+        # Identify the 10-value card in the hand
+        for card in self.hand:
+            if card.rank in rank_values:
+                return rank_values[card.rank]
+            
+        return None
+    
+    @property
     def card_ranks(self):
         return [card.card_value() for card in self.hand]
 
+    @property
     def can_split(self):
         return len(self.hand) == 2 and self.card_ranks[0] == self.card_ranks[1]
+    
+    @property
+    def has_doubled(self):
+        return self.wagered_amount == 2.0
